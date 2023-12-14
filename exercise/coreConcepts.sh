2e016ff424341e2c71a525a8232b825127ac9ac6
Nirmal, Subramanian <nirmal.subramanian@staples.com>
2023-12-13 22:49:22 -0500

    till port forwarding

---
+++ exercise/coreConcepts.sh
@@ -0,0 +1,50 @@
+# yum <package name> # to install appps in play with K8S environment
+# create an alias to kubectl
+alias k=kubectl
+alias 'kd=kubectl --dry-run=client -o=yaml'
+
+k options # to get the list of global command-line options (applies to all commands).
+k version --client -o=yaml # get kubectl client version
+k version -o=yaml # get kubectl client and server version 
+# using --client and --client=true is identical
+# not specifying the flag takes it default value (false in this case ||ar to --client=false)
+apt-get update
+apt-get install -y man #install manuals
+apt-get install -y tcpdump #install tcpdump
+apt-get install -y lsof # install lsof (is a command for LiSting Open Files)
+apt-get install -y procps # install procps (used for ps command) is a set of command line and full-screen utilities that provide information out of the pseudo-filesystem most commonly located at /proc
+apt-get install -y curl 
+
+# to get/open a shell to the pod. we can run the shell commands till we give (ctrl + d) / exit to exit out of the shell. 
+k exec --stdin --tty <pod-name> -- /bin/bash
+k exec -i -t <pod-name> -- /bin/bash
+k exec -i -t <pod-name> --container <container name> -- /bin/bash #if pod has more than one container
+k exec -i -t <pod-name> -c <container name> -- /bin/bash #if pod has more than one container
+# to run individual commands 
+k exec shell-demo -- env  #(list environment variables)
+kubectl exec shell-demo -- ps aux
+kubectl exec shell-demo -- ls /
+kubectl exec shell-demo -- cat /proc/1/mounts
+
+# to use the kubectl client to connect to multiple clusters/namespaces/users create contexts and switch between contexts
+k config --kube-config=<file name> #the context details are written to and read from this file
+# if u appect the <file name> to $KUBECONFIG (export KUBECONFIG="${KUBECONFIG};<file-path1>;<file-path-2>") we can skip passing the 
+#   --kubeconfig flag.
+k config view  # to view the available contexts and location, authorization for API server
+k config view --mninify # to view only the current context
+
+k auth --help # Inspect authorizations allowed.
+
+# create a proxy server (in background) that we can use to access API server via curl/wget
+k proxy -p=8080 & 
+# curl http://localhost:8080/api/  # API access by REST
+
+k get pod <pod name> --template='{{.spec.containers}}{{"\n"}}' # GO Template to print container array 
+K get pod <pod name> --template='{{(index .spec.containers 0)}}{{"\n"}}' # GO Template to print first element of container array
+
+
+
+# Forward local port (port on the machine where kubectl client is executed) to a port on pod {[LOCAL_PORT:]REMOTE_PORT}
+k port-forward mongo-7d96cb4cf-cl47r 28015:27017 & # (not mentioning local port system assigns a higher number port arandomly)
+# connect to mongo shell in pod from client on port 28015
+mongosh --port 28015
--- play-with-k8s.sh
+++ play-with-k8s.sh
@@ -4,6 +4,8 @@
  kubeadm init --apiserver-advertise-address $(hostname -i) --pod-network-cidr 10.5.0.0/16
 
 # OUTPUT from command 1 (CHECK THE USER. usually it is root. GO TO alternatively. part)
+
+# #### COPY the kubeadm command to add new nodes to the master
 Your Kubernetes control-plane has initialized successfully!
 
 To start using your cluster, you need to run the following as a regular user:
@@ -28,7 +30,16 @@ Then you can join any number of worker nodes by running the following on each as
 
 kubeadm join 192.168.0.8:6443 --token <<SOME TOKEN>> \
         --discovery-token-ca-cert-hash <<some Hash>>
+ ## After kubeadm iit command is run copy the token and discovery token hash. 
+ 
+ ##if missed get token using 
+    kubeadm token list
 
+## print the discovery token hash using  (need to install openssl and sha256sum). get the output of the command use in format "sha256:<<SHASUM value>>"
+openssl x509 -in /etc/kubernetes/pki/ca.crt -noout -pubkey | openssl rsa -pubin -outform DER 2>/dev/null | sha256sum | cut -d' ' -f1
+
+ ### you can generate a new token and get the hash using (the whole command to join)
+    kubeadm token create --print-join-command
 
 
 
